99fb3e3624e3c706f0ff6adca0e82c1eb8532d2f
holkarajitgyandev <139675885+holkarajitgyandev@users.noreply.github.com>
2025-01-05 21:49:27 +0530

    Update ProductCard.jsx

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '@/styles/ProductCard.module.css'
 import Image from 'next/image'
+import {CiHeart } from "react-icons/ci";
 
 const ProductCard = ({product}) => {
   return (
@@ -8,7 +9,7 @@ const ProductCard = ({product}) => {
         {/* <Image src={product.image} alt={product.title}></Image> */}
         <img src={product.image} alt={product.title} width={300} height={390}/>
         <div>
-        <h2>{product.title.slice(0,20)}</h2>
+        <div style={{display:'flex',justifyContent:'space-around'}}><h2>{product.title.slice(0,20)}</h2> <CiHeart size={25} /></div>
         <p><a href="">Sign In</a> or <a href="">Create an Account</a> to see pricing</p>
         </div>
 
